5553e442ee74f2edf70b151db5a4092a0e0d0ceb
EmreOren <m.emreoren@gmail.com>
2025-03-21 17:15:41 +0200

    Kaut kas jauns

--- src/main/java/lv/venta/Seminar2Application.java
+++ src/main/java/lv/venta/Seminar2Application.java
@@ -1,5 +1,6 @@
 package lv.venta;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
@@ -17,56 +18,65 @@ import lv.venta.repo.IGradeRepo;
 import lv.venta.repo.IProfessorRepo;
 import lv.venta.repo.IStudentRepo;
 
-
-
 @SpringBootApplication
 public class Seminar2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar2Application.class, args);
 	}
-	
 	@Bean
-	public CommandLineRunner testModelLayer(IStudentRepo studRepo,
-			IProfessorRepo profRepo, IGradeRepo grRep, ICourseRepo courseRepo)
+	public CommandLineRunner testModelLayer(IStudentRepo studRepo, 
+			IProfessorRepo profRepo, IGradeRepo grRepo, ICourseRepo courseRepo)
 	{
 		return new CommandLineRunner() {
 			
 			@Override
 			public void run(String... args) throws Exception {
-			Student st1 = new Student ("Pauls", "Staigataijs");
-			Student st2 = new Student ("Vins", "Dizelis");
-			Student st3 = new Student ("Janis", "Cena");
-			studRepo.save(st1);
-			studRepo.save(st2);
-			studRepo.save(st3);
+				Student s1 = new Student("Konstantīns", "Čmils");
+				Student s2 = new Student("Guna", "Kravale");
+				Student s3 = new Student("Sintija", "Ernštreite");
+				
+				studRepo.save(s1);
+				studRepo.save(s2);
+				studRepo.save(s3);
+				
+				Professor p1 = new Professor("Karina", "Šķirmante", Degree.magistra);
+				Professor p2 = new Professor("Kārlis", "Immers", Degree.magistra);
+				Professor p3 = new Professor("Vija", "Vagale", Degree.doktora);
+				Professor p4 = new Professor("Raita", "Rollande", Degree.doktora);
+				
+				//garais pieraksts
+				/*
+				profRepo.save(p1);
+				profRepo.save(p2);
+				profRepo.save(p3);
+				*/
+				//īsais pieraksts
+				profRepo.saveAll((Arrays.asList(p1, p2, p3, p4)));
 			
-			Professor p1 = new Professor("Karina", "Skirmate", Degree.magistra);
-			Professor p2 = new Professor("Karlis", "Immers", Degree.magistra);
-			Professor p3 = new Professor("Vija","Vagale",Degree.doktora);
-			
-		    profRepo.save(p1);
-		    profRepo.save(p2);
-		    profRepo.save(p3);
-			
-		    
-		    Course c1 = new Course("Programmaturas Inzenierija I",6,p1);
-		    Course c2 = new Course("Datus Bazes II",3,p3);
-		    Course c3 = new Course("Web tehnologijas",6,p2);
-		    
-		    courseRepo.saveAll(Arrays.asList(c1,c2,c3));
-		    
-			Grade g1 = new Grade(6, st1, c1);	
-			Grade g2 = new Grade(3, st2, c2);
-			Grade g3 = new Grade(8, st3, c3);
-			Grade g4 = new Grade(4, st1, c1);
-			Grade g5 = new Grade(7, st1, c2);
-			Grade g6 = new Grade(10, st1, c3);
-			Grade g7 = new Grade(1, st3, c1);
-			
-			grRep.saveAll(Arrays.asList(g1,g2,g3,g4,g5,g6,g7));
+				Course c1 = new Course("Programmatūras Inženierija I", 6, p1, p4);//pasniedz gan Karina, gan Raita
+				Course c2 = new Course("Datubāzes II", 3, p3);
+				Course c3 = new Course("Web tehnoloģijas", 6, p2);
+				Course c4 = new Course("Datu Struktūras un algoritmi", 3, p1);
+				courseRepo.saveAll(Arrays.asList(c1, c2, c3, c4));
+				
+				
+				p1.addCourse(c1);
+				p1.addCourse(c4);
+				p2.addCourse(c3);
+				p3.addCourse(c2);
+				p4.addCourse(c1);
+				profRepo.saveAll((Arrays.asList(p1, p2, p3, p4)));
+				
+				Grade g1 = new Grade(6, s1, c1);//Konstantīns nopelnīja 6 ProgInz I
+				Grade g2 = new Grade(9, s1, c2);//Konstantīns nopelnīja 9 Datubāzēs II
+				Grade g3 = new Grade(3, s2, c1);//Guna nopelnīja 3 ProgInz I
+				Grade g4 = new Grade(8, s2, c2);//Guna nopelnīja 8 Datubāzēs II
+				Grade g5 = new Grade(10, s2, c3);//Guna nopelnīja 10 Webteh
+				Grade g6 = new Grade(7, s3, c2);//Sintija nopelnīja 7 Datubāzēs II
+				Grade g7 = new Grade(5, s3, c3);//Sintija nopelnīja 5 Webteh
+				grRepo.saveAll(Arrays.asList(g1, g2, g3, g4, g5, g6, g7));
 			}
 		};
 	}
-
 }
